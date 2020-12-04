942514f746c1f8dc825957e472b339d7042300a3
Sinkem <61325685+Sinkem@users.noreply.github.com>
2020-12-04 13:03:35 +0100

    Update HolaMundo.java

--- ProyectoRamas/src/a/HolaMundo.java
+++ ProyectoRamas/src/a/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Mundo");
+		System.out.println("Wola Mundo");
 	}
 	
 	public static void imprimeSaludo() { 
